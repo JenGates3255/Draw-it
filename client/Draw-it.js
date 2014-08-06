aca48028f810f1f20ed2db9b90929cb251e6dab2
dlsso <supertonesfan@hotmail.com>
2014-08-06 17:56:26 -0600

    Changed drawing to be global

---
+++ client/Draw-it.js
@@ -0,0 +1,54 @@
+points = new Meteor.Collection('pointsCollection');
+var canvas;
+
+Deps.autorun( function () {
+  Meteor.subscribe('pointsSubscription');
+});
+
+Meteor.startup( function() {
+  canvas = new Canvas();
+
+  Deps.autorun( function() {
+    var data = points.find({}).fetch();
+    $('h2').hide();
+    if (canvas) {
+      canvas.draw(data);
+    }
+  });
+});
+
+Template.drawingSurface.title = function () {
+  return 'Draw with Me! (A Collaborative, Real-Time Drawing Environment) Works best in Chrome.';
+}
+
+Template.drawingSurface.events({
+  'click input': function (event) {
+    Meteor.call('clear', function() {
+      canvas.clear();
+    });
+  }
+})
+
+var markPoint = function() {
+  var offset = $('#canvas').offset();
+      points.insert({
+      x: (event.pageX - offset.left),
+      y: (event.pageY - offset.top)});
+}
+
+Template.canvas.events({
+  'click': function (event) {
+    markPoint();
+  },
+  'mousedown': function (event) {
+    Session.set('draw', true);
+  },
+  'mouseup': function (event) {
+    Session.set('draw', false);
+  },
+  'mousemove': function (event) {
+    if (Session.get('draw')) {
+      markPoint();
+    }
+  }
+});
